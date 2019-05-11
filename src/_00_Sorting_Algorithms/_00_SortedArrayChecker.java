16ee81a3500c82266c8b172e3b22f762dd137b87
DemoliciousDt <league@Thes-Mac-mini.local>
2019-05-11 12:18:14 -0700

    Began work on SortedArrayChecker

--- src/_00_Sorting_Algorithms/_00_SortedArrayChecker.java
+++ src/_00_Sorting_Algorithms/_00_SortedArrayChecker.java
@@ -6,13 +6,29 @@ public class _00_SortedArrayChecker {
 	//   and it returns a boolean.
 	//   The method returns true if the integer
 	//   array is in ascending order and false otherwise
-
+	protected static boolean intArraySorted(int integerList[]) {
+		for(int i = 0; i < integerList.length-1;i++) {
+			if(integerList[i] > integerList[i+1]) {
+				return false;
+			}
+		}
+		return true;
+	
+	}
 	
 	//2. Write a static method called doubleArraySorted. 
 	//   This method takes in an array of doubles
 	//   and it returns a boolean.
 	//   The method returns true if the double
 	//   array is in ascending order and false otherwise
+	protected static boolean doubleArraySorted(double[] doubleList) {
+		for(int i = 0; i < doubleList.length-1;i++) {
+			if(doubleList[i] > doubleList[i+1]) {
+				return false;
+			}
+		}
+		return true;
+	}
 
 	
 	//3. Write a static method called charArraySorted. 
@@ -21,6 +37,14 @@ public class _00_SortedArrayChecker {
 	//   The method returns true if the character
 	//   array is in alphabetical order and false otherwise
 	//   (You can compare characters just like integers)
+	protected static boolean charArraySorted(char[] characterList) {
+		for(int i = 0; i < characterList.length-1;i++) {
+			if(characterList[i] > characterList[i+1]) {
+				return false;
+			}
+		}
+		return true;
+	}	
 
 	
 	//4. Write a static method called stringArraySorted. 
@@ -29,5 +53,15 @@ public class _00_SortedArrayChecker {
 	//   The method returns true if the String
 	//   array is in alphabetical order and false otherwise
 	//   (Use the compareTo(String) method)
-
+	protected static boolean stringArraySorted(String[] stringList) {
+		for(int i = 0; i < stringList.length-1;i++) {
+			char[] stringAsChar1 = stringList[i].toCharArray();
+			char [] stringAsChar2 = stringList[i+1].toCharArray();
+			if(stringAsChar1[1] > stringAsChar2[1]) {
+				return false;
+			}
+		}
+		return true;
+	}
 }
+
