7bfed914823891a1e953c0e16f5c8842a172cbbd
ThirdTime <league@Mac-mini-3.local>
2019-08-31 12:12:57 -0700

    Finished all sorting methods, started sorting methods

--- src/_00_Sorting_Algorithms/BogoSorter.java
+++ src/_00_Sorting_Algorithms/BogoSorter.java
@@ -20,6 +20,25 @@ public class BogoSorter extends Sorter {
 	//1. Complete the sort method using the Bogo sort algorithm. 
 	@Override
 	void sort(int[] array, SortingVisualizer display) {
-
+		Random rand = new Random();
+		while(!arraySorted(array)) {
+			display.updateDisplay();
+			int rand1 = rand.nextInt(array.length);
+			int rand2 = rand.nextInt(array.length);
+			
+			int tempInt = array[rand1];
+			array[rand1] = array[rand2];
+			array[rand2] = tempInt;
+			
+		}
+	}
+	
+	boolean arraySorted(int[] array) {
+		for(int i = 0; i < array.length-1; i++) {
+			if(array[i] > array[i+1]) {
+				return false;
+			}
+		}
+		return true;
 	}
 }
--- src/_00_Sorting_Algorithms/MergeSorter.java
+++ src/_00_Sorting_Algorithms/MergeSorter.java
@@ -30,13 +30,13 @@ public class MergeSorter extends Sorter {
 			// 3. Create an integer called middle and set it
 			// equal to the half way point between low and high
 			int middle = (high + low) / 2;
-			
+
 			// 4. call the mergeSort method with low and middle
 			mergeSort(array, low, middle, display);
-			
+
 			// 5. call the mergeSort method with middle + 1 and high
 			mergeSort(array, middle + 1, high, display);
-			
+
 			// 6. copy the elements from the array into the temporary array,
 			// but only the elements from low to high inclusive
 			for (int i = low; i <= high; i++) {
@@ -50,43 +50,42 @@ public class MergeSorter extends Sorter {
 
 			// 8. while i is less than or equal to middle
 			// and j is less than or equal to high
-			while(i <= middle && j <= high) {
-				
+			while (i <= middle && j <= high) {
+				display.updateDisplay();
 				// 9. if temp array at i is less than or equal
 				// to temp array at j
-				if (tempIntArray[i]<=tempIntArray[j]) {
-					
+				if (tempIntArray[i] <= tempIntArray[j]) {
+
 					// 10. set array at k equal to temp array at i
 					array[k] = tempIntArray[i];
-					
+
 					// 11. increase i by 1
 					i++;
-					
+
 					// 13. else
+				} else {
+
+					// 14. set array at k equal to temp array at j
+					array[k] = tempIntArray[j];
+
+					// 15. increase j by 1
+					j++;
 				}
-					else {
-						
-						// 14. set array at k equal to temp array at j
-						array[k] = tempIntArray[j];
-						
-						// 15. increase j by 1
-						j++;
-						
-						// 16. increase k by 1
-						k++;
-						
-						// 17. make a while loop that runs while i is less than or equal to middle
-						while(i <= middle) {
-							
-							// 18. set array at k equal to temp array at i
-							array[k] = tempIntArray[i];
-							
-							// 19. increase k and i by 1
-							k++;
-							i++;
-						}
-					}
-				}
+				// 16. increase k by 1
+				k++;
+			}
+
+			// 17. make a while loop that runs while i is less than or equal to middle
+			while (i <= middle) {
+
+				// 18. set array at k equal to temp array at i
+				array[k] = tempIntArray[i];
+
+				// 19. increase k and i by 1
+				k++;
+				i++;
+			}
+
 		}
 
 	}
