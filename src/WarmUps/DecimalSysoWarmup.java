d9ee4cffd7d527bc7162fd6f85bdc4ec100511e1
DemoliciousDt <league@Thes-Mac-mini.local>
2019-09-07 10:35:01 -0700

    Create DecimalSysoWarmup.java

---
+++ src/WarmUps/DecimalSysoWarmup.java
@@ -0,0 +1,20 @@
+package WarmUps;
+
+public class DecimalSysoWarmup {
+	public static void main(String[] args) {
+		if (.1 + .2 == .3) {
+			System.out.print(1 / 2);
+		}
+		if (.2 + .2 == .4) {
+			System.out.print(2 / 2);
+		}
+		if(.3 + .2 == .5) {
+			System.out.print(3 / 2);
+		}
+		if(.4 + .2 == .6) {
+			System.out.print(4 / 2);
+		}
+		
+		System.out.println(.1+.2);
+	}
+}
