67313f527c360d251b9d5f6abcfd7f394dc7a872
League <league@WorthyMini-2.local>
2019-09-14 11:11:59 -0700

    Completed All Tests

--- src/_01_Searching_Algorithms/_04_SearchTest.java
+++ src/_01_Searching_Algorithms/_04_SearchTest.java
@@ -17,7 +17,7 @@ class _04_SearchTest {
 		String test2[] = { "nail polish", "two million", "PURPLE", "purple" };
 		assertEquals(3, _00_LinearSearch.linearSearch(test2, "purple"));
 		
-		String test3[] = { "fish", "oak tree", "OAK TREE", "cabbage" };
+		String test3[] = { "fish", "oak tree", "OAK TREE"};
 		assertEquals(2, _00_LinearSearch.linearSearch(test3, "OAK TREE"));
 	}
 
@@ -28,6 +28,13 @@ class _04_SearchTest {
 		int test1[] = { 1, 5, 8, 12, 18, 99, 100 };
 		assertEquals(2, _01_BinarySearch.binarySearch(test1, 0, 6, 8));
 		// searching for 8
+		
+		int test2[] = { 4, 7, 10, 459, 9444 };
+		assertEquals(2, _01_BinarySearch.binarySearch(test2, 0, 4, 10));
+		
+		int test3[] = { 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+		assertEquals(0, _01_BinarySearch.binarySearch(test3, 0, 12, 8));
+		
 	}
 
 	@Test
